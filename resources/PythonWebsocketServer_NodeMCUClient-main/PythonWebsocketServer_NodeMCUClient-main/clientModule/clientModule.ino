06acd11cff25af72a7a06ff1a6e23d49b275491b
Kevin Lu <lu_kevin@hotmail.com>
2022-12-04 21:06:58 -0500

    updated clientModule

--- resources/PythonWebsocketServer_NodeMCUClient-main/PythonWebsocketServer_NodeMCUClient-main/clientModule/clientModule.ino
+++ resources/PythonWebsocketServer_NodeMCUClient-main/PythonWebsocketServer_NodeMCUClient-main/clientModule/clientModule.ino
@@ -5,11 +5,13 @@
 #include <WebSocketsClient_Generic.h>
 #include <SocketIOclient_Generic.h>
 
+bool RECEIVED_DATE = false;
+
 const char* ssid = "4US2 WiFi"; //Enter SSID
 const char* password = ""; //Enter Password
 
 // Select the server IP address and port according to your local network
-IPAddress serverIP(192, 168, 1, 101);
+IPAddress serverIP(192, 168, 1, 102);
 uint16_t  serverPort = 5001;
 
 // Initalize SocketIO client
@@ -34,9 +36,11 @@ void socketIOEvent(const socketIOmessageType_t& type, uint8_t * payload, const s
     case sIOtype_EVENT:
       Serial.print("[IOc] Get event: ");
       Serial.println((char*) payload);
-      
       handleNewEvent((char*) payload);
 
+      // if (payload == 'date') {
+      //   // split date by
+      // }
       break;
     case sIOtype_PING:
       Serial.println("[IOc] Get PING");
@@ -64,9 +68,21 @@ void handleNewEvent(char* jsonEvent) {
   }
 
   if (strcmp(doc[0], "date") == 0) {
-    Serial.print("Received Date Event: ");
+    Serial.print("Received Event: date");
     const char* date = doc[1];
     Serial.println(date);
+    RECEIVED_DATE = true;
+    // Need to send to python
+    // Than recieve back value for that hour
+    // Possibly return all 24 data points and store in an array
+    // Start looping through and writing data points to operators
+    return;
+  }
+
+  if (strcmp(doc[0], "percent_max_output") == 0) {
+    Serial.print("Recieved Event: percent_max_output");
+    const int percentage = doc[1];
+    Serial.println(percentage);
     return;
   }
 
@@ -102,8 +118,37 @@ void setup()
 
 unsigned long messageTimestamp = 0;
 void loop() {
-  
+
   socketIO.loop();
+
+
+  if (RECEIVED_DATE) {
+    
+    // Package and send to server every 12.5 seconds
+    // When you recieve data back send over PWM to effect circuit
+
+    //   // creat JSON message for Socket.IO (event)
+    //   DynamicJsonDocument doc(1024);
+    //   JsonArray array = doc.to<JsonArray>();
+
+    //   // add evnet name
+    //   // Hint: socket.on('event_name', ....
+    //   array.add("output_value");
+    //   array.add("80");
+
+
+    //   String output;
+    //   serializeJson(doc, output);
+
+    //   // Send event
+    //   socketIO.sendEVENT(output);
+
+    //   // Print JSON for debugging
+    //   Serial.println(output);
+    return;
+  }
+
+  // For value in 24 data values 
 
   // uint64_t now = millis();
 
