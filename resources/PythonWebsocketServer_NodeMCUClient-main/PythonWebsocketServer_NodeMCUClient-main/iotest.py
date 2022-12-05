2f9db7384243d018f8eacc79b844a8e87a83110c
Kevin Lu <lu_kevin@hotmail.com>
2022-12-05 16:18:09 -0500

    commiting iotest

--- resources/PythonWebsocketServer_NodeMCUClient-main/PythonWebsocketServer_NodeMCUClient-main/iotest.py
+++ resources/PythonWebsocketServer_NodeMCUClient-main/PythonWebsocketServer_NodeMCUClient-main/iotest.py
@@ -10,11 +10,8 @@ from tkinter import Tk, Canvas
 ser = serial.Serial(port='COM4', baudrate=115200, timeout=1)
 
 while True:
-    #rint = random.randint(1,10)
+    rint = random.randint(1,10)
     # rint = 41
-    print(3)
-    ser.write((str(3) + '\r\n').encode())
-    time.sleep(1)
-    print(4)
-    ser.write((str(4) + '\r\n').encode())
+    print(rint)
+    ser.write((str(rint) + '\r\n').encode())
     time.sleep(1)
